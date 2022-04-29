47edbd63d458aab07a8fdb984ff69400a8324cea
Ruben Teles <75394884+RubenTeles@users.noreply.github.com>
2022-04-29 19:44:14 +0100

    Add files via upload

--- ft_so_long.c
+++ ft_so_long.c
@@ -6,13 +6,67 @@
 /*   By: rteles <rteles@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 21:57:29 by rteles            #+#    #+#             */
-/*   Updated: 2022/04/26 22:30:15 by rteles           ###   ########.fr       */
+/*   Updated: 2022/04/29 19:41:33 by rteles           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_so_long.h"
 #include <stdio.h>
 #include <unistd.h>
+
+ft_so_long(void)
+{
+	t_all	ctr;
+	t_vars	mlx;
+	t_img	img;
+	int		a;
+
+	a = 0;
+	img.path = "./Frames/Terriermon/Dead.xpm";
+	mlx.ptr = mlx_init();
+	if (!mlx.ptr)
+		exit(0);
+	mlx.win = mlx_new_window(mlx.ptr, 1920, 1080, "Jogo");
+	img.img = mlx_xpm_file_to_image(mlx.ptr, img.path, &img.width, &img.height);
+	mlx_put_image_to_window(mlx.ptr, mlx.win, img.img, 0, 0);
+	a = mlx_key_hook(mlx.win, key_hook, &mlx);
+	if (a == 1)
+	{
+		mlx_destroy_image(mlx.ptr, &img);
+	}
+	mlx_loop_hook(mlx.ptr, asjadsjadj, &mlx);
+	mlx_loop(mlx.ptr);
+	system("leaks -- so_long");
+}
+
+int iniciar_jogo(t_vars mlx)
+{
+	t_all	ctr;
+
+	mlx.ptr = mlx_init();
+	if (!mlx.ptr)
+		exit(0);
+	return (1);
+}
+
+void *meter_imagens_com_string(char a, t_vars mlx)
+{
+	t_img	img;
+	
+	if (a == '0')
+		img.path = "./Frames/Background/b0.xpm";
+	else if (a == '1')
+		img.path = "./Frames/Background/w0.xpm";
+	else if (a == 'C')
+		img.path = "./Frames/Menu/egg0.xpm";
+	else if (a == 'P')
+		img.path = "./Frames/Terriermon/S0.xpm";
+	else if (a == 'E')
+		img.path = "./Frames/Menu/p0.xpm";
+	else if (a == '\n' || a == '\0')
+		return (0);
+	return (mlx_xpm_file_to_image(mlx.ptr, img.path, &img.width, &img.height));
+}
 
 int	main(int argc, char **argv)
 {
